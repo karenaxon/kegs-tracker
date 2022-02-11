f8bccefe80ca850b0012faae6d1862aa1413b4cf
Kaxon <karenaxon@users.noreply.github.com>
2022-02-11 10:40:44 -0800

    Add Keg component.

---
+++ src/components/Keg.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props){
+  return(
+    <React.Fragment>
+      <div class="container">
+        <h2 class="text-uppercase">{props.name}</h2>
+        <h4>Brand: {props.brand}</h4>
+        <h4>Price: {props.price}</h4>
+        <h4>Alcohol Content: {props.alcoholContent}</h4>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.string,
+  alcoholContent: PropTypes.string
+}
